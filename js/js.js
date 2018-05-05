e3558ea9cf6c7ec83b96c2b3f9ae061d10e19300
Michael <Michael.hao25@gmail.com>
2018-05-05 16:20:35 +0800

    finish project.

--- js/js.js
+++ js/js.js
@@ -1,10 +1,34 @@
 var mySwiper = new Swiper('.banner .swiper-container', {
     pagination: '.banner .swiper-pagination',
-    loop:true,
-    paginationClickable :true,
+    loop: true,
+    paginationClickable: true,
 })
 var notice = new Swiper('.notice .swiper-container', {
-    loop:true,
-    slidesPerView : 1.5,
-    spaceBetween : 30
+    loop: true,
+    slidesPerView: 1.5,
+    spaceBetween: 30
 })
+
+
+try {
+    var a = document.querySelectorAll('.check-login .pic')[0]
+    a.onclick = function () {
+        console.log(1)
+        this.getElementsByTagName('input')[0].click();
+    }
+    $('.check-login  .pic input').on('change', function () {
+        var url;
+        $(this).parent().addClass('active');
+        if (window.createObjectURL != undefined) { // basic
+            url = window.createObjectURL(this.files[0]);
+        } else if (window.URL != undefined) { // mozilla(firefox)
+            url = window.URL.createObjectURL(this.files[0]);
+        } else if (window.webkitURL != undefined) { // webkit or chrome
+            url = window.webkitURL.createObjectURL(this.files[0]);
+        }
+        $('.check-login .pic img').attr('src', url);
+    });
+    // throw 'dada'
+} catch (error) {
+    console.log(error)
+}
