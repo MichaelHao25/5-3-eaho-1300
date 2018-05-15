f12aa246e5379b7da062b62f1bd3cbab5b5c991d
Michael <Michael.hao25@gmail.com>
2018-05-15 13:15:53 +0800

    update type page tabs.

--- js/js.js
+++ js/js.js
@@ -46,3 +46,22 @@ $('.javascript-reply').on('click',function() {
 $('.child-reply .close').on('click',function() {
     $(this).parent().slideUp();
 })
+
+
+$('*[data-js-tabs]').children().on('click', function() {
+	var tabs_obj = $(this).parent().attr('data-js-tabs');
+	$(tabs_obj).children().eq($(this).index()).show().siblings().hide();
+	$(this).addClass('active').siblings().removeClass('active');
+});
+$.each($('*[data-js-tabs]'), function(index, el) {
+	var arg = window.location.search;
+	if (arg != '') {
+		arg = arg.split('?')[1].split('=')[1];
+		console.log(arg);
+		$(el).children().eq(arg - 1).trigger('click');
+
+	} else {
+		$(el).children().first().addClass('active');
+		$($(el).attr('data-js-tabs')).children().eq(0).show().siblings().hide();;
+	}
+});
